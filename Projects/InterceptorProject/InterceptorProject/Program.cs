f3f4bdeb99083ee909514e2d027ae409ff34add6
Daniel Mertens <daniel.mertens@infosupport.com>
2024-02-06 21:14:41 +0100

    interceptors and SDK

---
+++ Projects/InterceptorProject/InterceptorProject/Program.cs
@@ -0,0 +1,26 @@
+﻿// User code
+Console.WriteLine("original");
+Console.WriteLine("original");
+
+// Generated code
+namespace InterceptorProject
+{
+    public static class Interceptor
+    {
+        [System.Runtime.CompilerServices.InterceptsLocation(
+            filePath: @"C:\Users\DanielM\Documents\ISKA\Dotnet 8\Projects\InterceptorProject\InterceptorProject\Program.cs",
+            line: 3,
+            column: 9)]
+        public static void InterceptWriteLine(string? message)
+        {
+            Console.WriteLine($"INTERCEPTED! Original message was '{message}'");
+        }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    file sealed class InterceptsLocationAttribute(string filePath, int line, int column) : Attribute;
+}
+
